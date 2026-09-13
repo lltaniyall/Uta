d2d219be19fcf41b63e896aa1c9b41732f5f8c85
lltaniyall <lltaniyall.email@gmail.com>
2026-09-14 02:58:58 +0900

    Update site-config.js

--- data/site-config.js
+++ data/site-config.js
@@ -1,7 +1,7 @@
 const SITE_CONFIG = {
   socials: [
     { label: "YouTube", sub: "VIDEOS / STREAMS", url: "https://youtube.com/@lltaniyall?sub_confirmation=1" },
-    { label: "Twitter(現X", sub: "TWITTER", url: "https://x.com/lltaniyall" },
+    { label: "Twitter（現X）", sub: "TWITTER", url: "https://x.com/lltaniyall" },
     { label: "BOOTH", sub: "SHOP", url: "https://x.com/lltaniyall" },
     { label: "Bluesky", sub: "SOCIAL", url: "https://x.com/lltaniyall" },
     { label: "LINK 5", sub: "SOCIAL", url: "https://x.com/lltaniyall" }
