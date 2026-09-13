a62c524a5dd8aadf2774d9559194dbbebd6d7129
lltaniyall <lltaniyall.email@gmail.com>
2026-09-14 03:10:58 +0900

    Update songs.js

--- songs.js
+++ songs.js
@@ -1,7 +1,1 @@
-// ★ 配信や曲を増やすときは、基本的にこのファイルだけ編集します。
-const ARCHIVES=[
- {id:"live-001",date:"2026-09-01",title:"歌枠 #01 / DEMO",videoId:"M7lc1UVf-VE",songs:[
-  {title:"オープニング",time:0,type:"talk"},{title:"KING",time:75,type:"song"},{title:"ロウワー",time:245,type:"song"},{title:"MC",time:490,type:"talk"},{title:"ヴィラン",time:765,type:"song"}]},
- {id:"live-002",date:"2026-08-12",title:"夜のまったり歌枠 / DEMO",videoId:"M7lc1UVf-VE",songs:[
-  {title:"KING",time:332,type:"song"},{title:"怪獣の花唄",time:710,type:"song"},{title:"酔いどれ知らず",time:1050,type:"song"}]}
-];
+
