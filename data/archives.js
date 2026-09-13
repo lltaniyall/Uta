2240d889790ce239464598f8475c34cce0a1cbb9
lltaniyall <lltaniyall.email@gmail.com>
2026-09-14 01:10:37 +0900

    Add files via upload

---
+++ data/archives.js
@@ -0,0 +1,66 @@
+const ARCHIVES = [
+  {
+    "date": "2026-12-21",
+    "title": "冬の夜歌 / DEMO",
+    "videoId": "M7lc1UVf-VE",
+    "songs": [
+      {
+        "title": "オープニング",
+        "time": 0,
+        "type": "talk"
+      },
+      {
+        "title": "KING",
+        "time": 321,
+        "type": "song"
+      },
+      {
+        "title": "ロウワー",
+        "time": 702,
+        "type": "song"
+      },
+      {
+        "title": "MC",
+        "time": 980,
+        "type": "talk"
+      },
+      {
+        "title": "ヴィラン",
+        "time": 1111,
+        "type": "song"
+      }
+    ]
+  },
+  {
+    "date": "2026-12-23",
+    "title": "深夜にゆったり歌う枠 / DEMO",
+    "videoId": "M7lc1UVf-VE",
+    "songs": [
+      {
+        "title": "オープニング",
+        "time": 0,
+        "type": "talk"
+      },
+      {
+        "title": "怪獣の花唄",
+        "time": 245,
+        "type": "song"
+      },
+      {
+        "title": "酔いどれ知らず",
+        "time": 622,
+        "type": "song"
+      },
+      {
+        "title": "MC",
+        "time": 955,
+        "type": "talk"
+      },
+      {
+        "title": "KING",
+        "time": 1284,
+        "type": "song"
+      }
+    ]
+  }
+];
