8fe8e49cec725a9c89e48e5010d6a25cda0ea9cc
lltaniyall <lltaniyall.email@gmail.com>
2026-09-14 02:41:25 +0900

    Update site-config.js

--- data/site-config.js
+++ data/site-config.js
@@ -1,8 +1,8 @@
 const SITE_CONFIG = {
   socials: {
     youtube: "https://youtube.com/@lltaniyall?sub_confirmation=1",
-    Twitter(現X): "https://x.com/lltaniyall",
-    booth: "",
-    bluesky: ""
+    x: "https://x.com/lltaniyall",
+    booth: "https://x.com/lltaniyall",
+    bluesky: "https://x.com/lltaniyall"
   }
 };
